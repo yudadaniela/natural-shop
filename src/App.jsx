6359bf08103e3ad82e1ac8af23cf24b2682f410e
Daniela Nieto <yudadaniela@hotmail.com>
2025-09-03 14:28:04 -0500

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from "./Pages/Home"
 import Shop from "./Pages/Shop"
+import About from "./Pages/About";
 import './App.css'
 
 
 function App() {
  
   return (
-   <Router>
+   
     <Routes>
      <Route path='/' element={<Home/>}/>
      <Route path='/shop' element={<Shop/> }/>
+     <Route path='/about' element={<About/> }/>
     </Routes>
-   </Router>
+  
   )
 }
 
--- src/Componets/Header.jsx
+++ src/Componets/Header.jsx
@@ -1,28 +1,42 @@
 import React from "react";
 import Search from "./Search";
+import { useNavigate } from "react-router-dom";
 
-const Header = () => (
-  <header className="bg-background-secondary grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-6 p-6  h-20 items-center">
-    <h1>Natural Shop</h1>
-    <div> 
-    <Search />
-    </div>
-    <div>
-      <button className="bg-background-secondary text-primary-700 px-4 py-2 hover:bg-accent hover:text-white rounded">
-        Inicio
-      </button>
-      <button className="bg-background-secondary text-primary-500 px-4 py-2 hover:bg-accent hover:text-white rounded">
-        Compra
-      </button>
-      <button className="bg-background-secondary text-primary-500 px-4 py-2 hover:bg-accent hover:text-white rounded">
-        Nosotros
-      </button>
-      <button className="bg-background-secondary text-primary-500 px-4 py-2 hover:bg-accent hover:text-white rounded">
-        Contactanos
-      </button>
-    </div>
-    
-  </header>
-);
+const Header = () => {
+  const navigate = useNavigate();
+
+  function GoToprincipalPag() {
+    navigate('/');
+  }
+    function GoToShop() {
+    navigate('/shop');
+  }
+    function GoToAbout() {
+    navigate('/about');
+  }
+
+  return (
+    <header className="bg-background-secondary grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-6 p-6  h-20 items-center">
+      <h1>Natural Shop</h1>
+      <div>
+        <Search />
+      </div>
+      <div>
+        <button onClick={GoToprincipalPag} className="bg-background-secondary text-primary-700 px-4 py-2 hover:bg-accent hover:text-white rounded">
+          Inicio
+        </button>
+        <button onClick={GoToShop} className="bg-background-secondary text-primary-500 px-4 py-2 hover:bg-accent hover:text-white rounded">
+          Comprar
+        </button>
+        <button onClick={GoToAbout} className="bg-background-secondary text-primary-500 px-4 py-2 hover:bg-accent hover:text-white rounded">
+          Acerca de nosotros
+        </button>
+        <button className="bg-background-secondary text-primary-500 px-4 py-2 hover:bg-accent hover:text-white rounded">
+          Contactanos
+        </button>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
